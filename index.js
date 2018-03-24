140a1bac8e1431dac56d2887eb2d7427c7ede68a
Zichao Zang <zang.code@gmail.com>
2018-03-24 16:48:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,19 @@ function add2(n) {
   const two = 2
 }
 
+  describe('add2(n)', function() {
+    it('adds two to n', function() {
+      const n = Math.floor(Math.random() * 1000)
+      expect(window.add2(n)).toEqual(n + 2)
+    })
+  })
+
+
+
+
+
+
+
 var funkyFunction = function() {
   return function() {
     return "FUNKY!"
